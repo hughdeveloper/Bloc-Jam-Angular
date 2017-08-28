474a3df211830976a3ba1da0a6080e396fce88db
Hugh Bethell <hbethell@outlook.com>
2017-08-28 13:47:18 -0700

    checkpoint 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,2 +1,33 @@
-// the first argument is the presribed name of the module, the second argument is injecting an empty array
-angular.module('blocJams', []);
+(function() {
+	//$locationProvider configures tha application path.
+	function config($stateProvider, $locationProvider) {
+	//locationProvider automatically adds in #! (hashbang mode) to the url which is showing that the page was loaded by javascript. Ther $locationProvider code below disables this.
+		$locationProvider
+			.html5Mode ({
+			//by setting enabled to true in the html5mode we are disabling the hashbang
+				enabled: true,
+			//by setting the requireBase to false we are helping avoid $location errors. this has nothing to do with disabling the hashbang.
+				requireBase: false
+		});
+		
+		// determines the number of properties for a state
+		$stateProvider
+			.state('landing', {
+			url: '/',
+			templateUrl: '/templates/landing.html'
+		})
+			.state('album', {
+			url: '/album',
+			templateUrl: '/templates/album.html'
+		});
+		
+		
+		
+		
+	}
+	
+	// the first argument is the prescribed name of the module, the second argument is injecting an array that is filled with a list of external modules (extensions that angular can use)
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config);
+})();
