f3d0b6ea24b50906563c762167c1a57a267eb226
Hugh Bethell <hbethell@outlook.com>
2017-09-06 13:31:59 -0700

    checkpoint 5 assignment update

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,10 +1,11 @@
 (function() {
 	function AlbumCtrl() {
 		this.albumData = albumPicasso.songs;
+		this.titleData = albumPicasso.title;
+		this.artistData = albumPicasso.artist;
+		this.albumArtUrlData = albumPicasso.albumArtUrl;
+		this.yearRecordData = albumPicasso.year + ' ' + albumPicasso.label;
 	}
-	
-	
-
 	angular
 		.module('blocJams')
 	
