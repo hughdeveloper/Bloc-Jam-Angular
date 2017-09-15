6b57bed0c54c1a0f045e97b626be22e11fb4527c
Hugh Bethell <hbethell@outlook.com>
2017-09-15 15:31:48 -0700

    checkpoint 7

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,11 +1,12 @@
 (function() {
-	function AlbumCtrl(Fixtures) {
+	function AlbumCtrl(Fixtures, SongPlayer) {
 		this.albumData = Fixtures.getAlbum();
+		this.songPlayer = SongPlayer;
 	}
 	angular
 		.module('blocJams')
 	
 	//name of the controller and then the callback function
 	//added fixtures to the array of dependencies. We can now use the service within the controller
-		.controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+		.controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
